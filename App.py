ad211981ee844e2d2cca2e70198dab9c28af9a7f
Rahim Siddiq <roshsiddiq@gmail.com>
2025-07-24 23:32:37 -0700

    Ready for merging with main

---
+++ App.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Breast Cancer Prognosis Launcher",
+    layout="centered"
+)
+
+st.title("Welcome to Onco-Logic")
+st.write("Use the sidebar to navigate between pages.")
